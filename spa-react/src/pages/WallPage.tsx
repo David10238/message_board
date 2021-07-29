22217d4873ff8726d8e9689038a733e48cc59b98
David Lukens <davidqcy02@gmail.com>
2021-07-29 12:36:28 -0500

    draft a post input and button

--- spa-react/src/pages/WallPage.tsx
+++ spa-react/src/pages/WallPage.tsx
@@ -1,19 +1,9 @@
-import React from "react";
 import "./../styles/base.scss";
 import "./../styles/feed.scss";
 
 interface PostProps {
   name: String;
   body: String;
-}
-
-function Post(props: PostProps) {
-  return (
-    <div className="feed-item">
-      <div>{props.name}</div>
-      <div>{props.body}</div>
-    </div>
-  );
 }
 
 const posts: Array<PostProps> = [
@@ -31,11 +21,47 @@ const posts: Array<PostProps> = [
   },
 ];
 
+function Post(props: PostProps) {
+  return (
+    <div className="feed-item">
+      <div>{props.name}</div>
+      <div>{props.body}</div>
+    </div>
+  );
+}
+
+function PostInput() {
+  var input: String | null = null;
+  function onInput(textInput: React.FormEvent<HTMLDivElement>) {
+    input = textInput.currentTarget.innerText;
+  }
+
+  function doPost() {
+    const inputCache = input;
+    if (inputCache != null) {
+      console.log(inputCache);
+      document.getElementById("post-input")!!.innerText = "";
+    }
+    input = null;
+  }
+
+  return (
+    <div className="feed-item">
+      <div id="post-input" role="textbox" contentEditable onInput={onInput} />
+      <button id="post-button" onClick={doPost}>
+        Post
+      </button>
+    </div>
+  );
+}
+
 export default function WallPage() {
   return (
     <div id="feed-page">
       <div>
+        <PostInput />
         {posts.map((post) => (
+          //todo bind to unique ids
           <Post name={post.name} body={post.body} />
         ))}
       </div>
