bc68eb6392ea752b19a9d3ea3773e9e22e93108a
David Lukens <davidqcy02@gmail.com>
2021-07-29 14:24:18 -0500

    add spanish to login page. need to switch to redux

--- spa-react/src/pages/LoginPage.tsx
+++ spa-react/src/pages/LoginPage.tsx
@@ -1,19 +1,36 @@
 import React from "react";
+import { useForceUpdate } from "../reactUtil/Hooks";
+import { lang, Languages, setLanguage } from "../state/LanguageState";
 import "./../styles/base.scss";
 import "./../styles/login.scss";
 
+function LanguageButton(props: { language: Languages; update: () => void }) {
+  function updateLang() {
+    setLanguage(props.language);
+    props.update();
+  }
+  return <button onClick={updateLang}>{props.language}</button>;
+}
+
 export default function LoginPage() {
+  const forceUpdate = useForceUpdate();
   return (
     <div id="login-page">
       <div id="login-form">
-        <input value="Email" className="login-input" />
-        <input value="Password" className="login-input" />
-        <button id="login-button">Log In</button>
+        <input value={lang.auth_email} className="login-input" />
+        <input value={lang.auth_password} className="login-input" />
+        <button id="login-button">{lang.auth_login}</button>
         <a id="forgot-password-link" href="/forgotpassword">
-          Forgot Password?
+          {lang.auth_forgotPassword}
         </a>
         <div id="divider" />
-        <button id="new-account-button">Create New Account</button>
+        <div>
+          <button id="new-account-button">{lang.auth_createNew}</button>
+        </div>
+        <div>
+          <LanguageButton language={Languages.ENGLISH} update={forceUpdate} />
+          <LanguageButton language={Languages.SPANISH} update={forceUpdate} />
+        </div>
       </div>
     </div>
   );
---
+++ spa-react/src/state/LanguageState.ts
@@ -0,0 +1,44 @@
+interface Language {
+  languageName: string;
+  auth_email: string;
+  auth_password: string;
+  auth_login: string;
+  auth_forgotPassword: string;
+  auth_createNew: string;
+}
+
+const english: Language = {
+  languageName: "English",
+  auth_email: "Email",
+  auth_password: "Password",
+  auth_login: "Log in",
+  auth_forgotPassword: "Forgot password?",
+  auth_createNew: "Create new account",
+};
+
+const spanish: Language = {
+  languageName: "Español",
+  auth_email: "Correo electrónico",
+  auth_password: "Contraseña",
+  auth_login: "Iniciar sesión",
+  auth_forgotPassword: "¿Olvidaste tu contraseña?",
+  auth_createNew: "Crear cuenta nueva",
+};
+
+export enum Languages {
+  ENGLISH = "English",
+  SPANISH = "Español",
+}
+
+export let lang = english;
+
+export function setLanguage(langage: Languages) {
+  switch (langage) {
+    case Languages.ENGLISH:
+      lang = english;
+      break;
+    case Languages.SPANISH:
+      lang = spanish;
+      break;
+  }
+}
