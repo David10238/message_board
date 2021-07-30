ad411e2acdd014936d8092a60399930622c0392a
David Lukens <davidqcy02@gmail.com>
2021-07-30 10:48:27 -0500

    add auth routes for api

--- api-gin/auth-routes.go
+++ api-gin/auth-routes.go
@@ -3,5 +3,34 @@ package main
 import "github.com/gin-gonic/gin"
 
 func AuthRoutes(server *gin.Engine) {
+	server.DELETE("/auth/delete")
+	server.PATCH("/auth/changePassword")
+	server.PATCH("/auth/refreshToken")
+	server.GET("/auth/doesNameExist")
+	server.GET("/auth/token")
+	server.POST("/auth/create")
+}
+
+func deleteUser() {
+
+}
+
+func changePassword() {
+
+}
+
+func refreshPassword() {
+
+}
+
+func doesNameExist() {
+
+}
+
+func getToken() {
+
+}
+
+func createNewUser() {
 
 }
