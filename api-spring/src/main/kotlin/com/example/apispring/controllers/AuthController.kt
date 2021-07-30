69f787bd3247a550900e172fd8b0d36e2856d35c
David Lukens <davidqcy02@gmail.com>
2021-07-30 12:06:23 -0500

    add routes for auth controller

--- api-spring/src/main/kotlin/com/example/apispring/controllers/AuthController.kt
+++ api-spring/src/main/kotlin/com/example/apispring/controllers/AuthController.kt
@@ -1,6 +1,5 @@
 package com.example.apispring.controllers
 
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -9,7 +8,34 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class AuthController {
     @RequestMapping(value = ["/auth/delete"], method = [RequestMethod.DELETE])
-    fun deleteUser(@RequestHeader username: String, @RequestHeader password: String): String {
-        return "Deletion not implemented: $username, $password"
+    fun deleteUser(@RequestHeader username: String, @RequestHeader password: String) {
+    }
+
+    @RequestMapping(value = ["/auth/changePassword"], method = [RequestMethod.PATCH])
+    fun changePassword(
+        @RequestHeader username: String,
+        @RequestHeader password: String,
+        @RequestHeader newPassword: String
+    ) {
+    }
+
+    @RequestMapping(value = ["/auth/refreshToken"], method = [RequestMethod.PATCH])
+    fun refreshToken(@RequestHeader username: String, @RequestHeader token: String) {
+
+    }
+
+    @RequestMapping(value = ["/auth/doesNameExist"], method = [RequestMethod.GET])
+    fun doesNameExist(@RequestHeader username: String) {
+
+    }
+
+    @RequestMapping(value = ["/auth/token"], method = [RequestMethod.GET])
+    fun getToken(@RequestHeader username: String, @RequestHeader password: String) {
+
+    }
+
+    @RequestMapping(value = ["/auth/create"], method = [RequestMethod.POST])
+    fun createNewUser(@RequestHeader username: String, @RequestHeader password: String) {
+
     }
 }
--- api-spring/src/main/kotlin/com/example/apispring/models/Json.kt
+++ api-spring/src/main/kotlin/com/example/apispring/models/Json.kt
@@ -1,2 +1,9 @@
 package com.example.apispring.models
 
+import com.google.gson.Gson
+import kotlin.reflect.KClass
+
+private val GSON = Gson()
+
+fun toJson(dat:Any) = GSON.toJson(dat)
+fun<T> fromJSon(string:String, clazz:Class<T>):T = GSON.fromJson(string, clazz)
