c53464cbf828385a3eba0bf5378e84bb25c277b9
David Lukens <davidqcy02@gmail.com>
2021-07-30 19:30:35 -0500

    run code reformat

--- api-spring/src/main/kotlin/com/example/apispring/models/AuthModels.kt
+++ api-spring/src/main/kotlin/com/example/apispring/models/AuthModels.kt
@@ -2,6 +2,6 @@ package com.example.apispring.models
 
 import com.example.apispring.client.makeRawSpringResponse
 
-class Id(val id: Int?, val code: Int){
+class Id(val id: Int?, val code: Int) {
     fun asResponse() = makeRawSpringResponse(code, emptyList(), "")
 }
